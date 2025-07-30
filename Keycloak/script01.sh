b4ecfdbd58a9c84ec880f0b8a91e1fdb26a6b1e8
ehjeong95 <98686333+ehjeong95@users.noreply.github.com>
2025-07-30 14:31:32 +0900

    Update script01.sh

--- Keycloak/script01.sh
+++ Keycloak/script01.sh
@@ -2,6 +2,6 @@ kubectl delete namespace keycloak && \
 kubectl create namespace keycloak && \
 helm install euc . -n keycloak && \
 cd /etc/kubernetes/EUC/Cert/ && \
-kubectl create secret tls keycloak-tls --cert=tls.crt --key=tls.key -n keycloak && \
+kubectl create secret tls keycloak-tls --cert=fullchain.pem --key=privkey.pem -n keycloak && \
 kubectl get all -n keycloak && \
 cd /etc/kubernetes/EUC/Keycloak
